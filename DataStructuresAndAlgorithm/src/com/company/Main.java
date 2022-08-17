2c6eae5dcc5072ea1f64afea7552477c51b559c7
kamadi <eugenekamadi@yahoo.com>
2022-08-17 09:51:21 +0300

    smallets subarray with a greater sum

--- DataStructuresAndAlgorithm/src/com/company/Main.java
+++ DataStructuresAndAlgorithm/src/com/company/Main.java
@@ -14,6 +14,15 @@ public class Main {
         System.out.println("averages is: "+ Arrays.toString(results));
         System.out.println("********************************************************************************************");
 
+        final int value = 2;
+        int maxSum = maxSumSubArray(new int[]{2, 3, 4, 1, 5},value);
+        System.out.println("max sum is:" + maxSum);
+
+        System.out.println("********************************************************************************************");
+
+        System.out.println("smallest sub array is: "+smallestSubArray(new int[]{2, 1, 5, 2, 3, 2}, 7));
+
+
 
 
     }
@@ -37,9 +46,44 @@ public class Main {
         return result;
     }
 
+    static int maxSumSubArray(int[]arr, int j){
+        int maxSum=0;
+        int windowStart=0;
+        int windowSum=0;
+
+        for (int windowEnd = 0; windowEnd<arr.length;windowEnd++){
+            windowSum+=arr[windowEnd];
+            if(windowEnd >= j - 1){
+                if(maxSum <= windowSum){
+                    maxSum = windowSum;
+                }
+                windowSum-=arr[windowStart];
+                windowStart++;
+            }
+        }
+        return maxSum;
+    }
 
 
+    static int smallestSubArray(int[] arr, int S){
+        /*revisit logic... Time complexity is O(N+N) which is equivalent to O (N) */
 
+        int windowStart=0;
+        int subArrayCount = 0;
+        int subArraySum=0;
+
+        for( int windowEnd =0; windowEnd < arr.length; windowEnd++){
+
+            subArraySum+=arr[windowEnd];
+
+            while(subArraySum >= S){
+                subArrayCount = (windowEnd - windowStart + 1);
+                subArraySum -= arr[windowStart];
+                windowStart++;
+            }
+        }
+        return subArrayCount ;
+    }
 
 
 }
