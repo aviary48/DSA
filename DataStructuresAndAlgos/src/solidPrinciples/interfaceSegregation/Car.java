1bd23ded004c57a356187b36cd47f487337c08b0
Eugene Kamadi <Kamadi>
2024-01-11 15:44:38 +0300

    Interface Segregation Principle

---
+++ DataStructuresAndAlgos/src/solidPrinciples/interfaceSegregation/Car.java
@@ -0,0 +1,23 @@
+package solidPrinciples.interfaceSegregation;
+
+public class Car implements VehicleFunctions{
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void refuel() {
+
+    }
+
+    @Override
+    public void openDoors() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
